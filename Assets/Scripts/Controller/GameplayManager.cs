4bb89658e2c4f7575bb62c157d1d93c114f53590
Han <mreyhan693@gmail.com>
2019-10-27 12:24:20 +0700

    Stamina Bar Synchronization
    Stamina Bar now will updated accordingly to the updatedStamina value.

---
+++ Assets/Scripts/Controller/GameplayManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // GameObject player;
+    PlayerController playerController;
+    private Slider staminaBar;
+    void Start()
+    {
+        staminaBar = GameObject.Find("StaminaBar").GetComponent<Slider>();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController != null) staminaBar.value = playerController.updatedStamina;
+        else Debug.Log("NULL");
+    }
+}
