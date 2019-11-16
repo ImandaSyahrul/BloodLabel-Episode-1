36320a705a8f287e8bcee1def8b16e598662efde
ImandaSyahrul <ImandaSyahrul@github.com>
2019-11-16 12:14:44 +0700

    Added Volume and FullscreenToggle
    Volume works fine in editor but didnt work in application

--- Assets/Scripts/Controller/SettingsHandler.cs
+++ Assets/Scripts/Controller/SettingsHandler.cs
@@ -9,12 +9,18 @@ public class SettingsHandler : MonoBehaviour
 {
 	public GameObject[] settingPanels;
 	public AudioMixer audioMixer;
+	public AudioMixerGroup[] mixerGroups;
+	
 	private int currIndex;
 
 	Resolution[] resolutions;
 	int currentResolutionIndex;
 
-	public Text viewValues;
+	[SerializeField] private Text viewToggle;
+	[SerializeField] private Text viewValues;
+	[SerializeField] private Text[] groupValues;
+	private float[] mixerValues;
+
 	List<string> optionsResolution;
 
 
@@ -22,6 +28,7 @@ public class SettingsHandler : MonoBehaviour
 	void Start()
     {
 		currIndex = 0;
+		mixerValues = new float[3] {0.0f, 0.0f, 0.0f};
 
 		for (int i = 0; i < settingPanels.Length; i++)
 		{
@@ -49,19 +56,26 @@ public class SettingsHandler : MonoBehaviour
 		}
 
 		viewValues.text = Screen.currentResolution.width + "x" + Screen.currentResolution.height;
+		viewToggle.text = (Screen.fullScreen) ? "Fullscreen" : "Windowed";
+		for(int i = 0; i < mixerValues.Length; i++)
+		{
+			groupValues[i].text = ((int)(mixerValues[i]+80.0f)).ToString();
+		}
+		
+		
 
 	}
 
     // Update is called once per frame
     void Update()
     {
-		for(int i = 0; i< settingPanels.Length; i++)
+		for (int i = 0; i < settingPanels.Length; i++)
 		{
-			if(i == currIndex) settingPanels[i].GetComponent<Image>().color = new Vector4(1.0f, 1.0f, 1.0f, 5.0f);
+			if (i == currIndex) settingPanels[i].GetComponent<Image>().color = new Vector4(1.0f, 1.0f, 1.0f, 3.0f);
 			else settingPanels[i].GetComponent<Image>().color = new Vector4(1.0f, 1.0f, 1.0f, 0.0f);
 		}
 
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			currIndex -= 1;
 			if (currIndex < 0) currIndex = 4;
@@ -73,25 +87,69 @@ public class SettingsHandler : MonoBehaviour
 			if (currIndex > 4) currIndex = 0;
 		}
 
-
+		if (currIndex < 2)
+		{
+			if (currIndex == 0)
+			{
+				if (Input.GetKeyDown(KeyCode.LeftArrow))
+				{
+					if (currentResolutionIndex > 0)
+					{
+						currentResolutionIndex -= 1;
+						Screen.SetResolution(resolutions[currentResolutionIndex].width, resolutions[currentResolutionIndex].height, false);
+						viewValues.text = optionsResolution[currentResolutionIndex];
+					}
+				}
+				else if (Input.GetKeyDown(KeyCode.RightArrow))
+				{
+					if (currentResolutionIndex < resolutions.Length)
+					{
+						currentResolutionIndex += 1;
+						Screen.SetResolution(resolutions[currentResolutionIndex].width, resolutions[currentResolutionIndex].height, false);
+						viewValues.text = optionsResolution[currentResolutionIndex];
+					}
+				}
+			}
+			else if (currIndex == 1)
+			{
+				if (Input.GetKeyDown(KeyCode.LeftArrow))
+				{
+					if (Screen.fullScreen)
+					{
+						Screen.fullScreen = false;
+						viewToggle.text = "Windowed";
+					}
+				}
+				
+				if (Input.GetKeyDown(KeyCode.RightArrow))
+				{
+					if (!Screen.fullScreen)
+					{
+						Screen.fullScreen = true;
+						viewToggle.text = "Fullscreen";
+					}
+				}
+			}
+		}
+		else
+		{
+			string groupName = mixerGroups[currIndex-2].ToString();
+			Debug.Log(groupName);
 			if (Input.GetKeyDown(KeyCode.LeftArrow))
 			{
-				if (currentResolutionIndex > 0)
-				{
-					currentResolutionIndex -= 1;
-					Screen.SetResolution(resolutions[currentResolutionIndex].width, resolutions[currentResolutionIndex].height, false);
-					viewValues.text = optionsResolution[currentResolutionIndex];
-				}
+				mixerValues[currIndex - 2] -= 2.0f;
+				if (mixerValues[currIndex - 2] < -80.0f) mixerValues[currIndex - 2] = -80.0f;
+				groupValues[currIndex - 2].text = ((int)(mixerValues[currIndex - 2] + 80.0f)).ToString();
+				setVolume(groupName, mixerValues[currIndex - 2]);
 			}
 			else if (Input.GetKeyDown(KeyCode.RightArrow))
 			{
-				if (currentResolutionIndex < resolutions.Length)
-				{
-					currentResolutionIndex += 1;
-					Screen.SetResolution(resolutions[currentResolutionIndex].width, resolutions[currentResolutionIndex].height, false);
-					viewValues.text = optionsResolution[currentResolutionIndex];
-				}
+				mixerValues[currIndex - 2] += 2.0f;
+				if (mixerValues[currIndex - 2] > 20.0f) mixerValues[currIndex - 2] = 20.0f;
+				groupValues[currIndex - 2].text = ((int)(mixerValues[currIndex - 2] + 80.0f)).ToString();
+				setVolume(groupName, mixerValues[currIndex - 2]);
 			}
+		}
 		
 			
 		
@@ -103,28 +161,11 @@ public class SettingsHandler : MonoBehaviour
 		Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
 	}
 
-	public void setMasterVolume(float vol)
+	public void setVolume(string groupName,float vol)
 	{
-		audioMixer.SetFloat("master", vol);
+		audioMixer.SetFloat(groupName, vol);
 	}
 
-	public void setBGMVolume(float vol)
-	{
-		audioMixer.SetFloat("background", vol);
-	}
+	
 
-	public void setSFXVolume(float vol)
-	{
-		audioMixer.SetFloat("effect", vol);
-	}
-
-	public void setCharacterVolume(float vol)
-	{
-		audioMixer.SetFloat("voice", vol);
-	}
-
-	public void SetFullscreen(bool isFullscreen)
-	{
-		Screen.fullScreen = isFullscreen;
-	}
 }
